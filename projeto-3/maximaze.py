59f88262e58b5d7aaa846bd599ab7d044a80c0b5
Lucas Burgos <lucaspradocb@gmail.com>
2018-12-02 09:48:31 -0300

    final

--- projeto-3/maximaze.py
+++ projeto-3/maximaze.py
@@ -1,14 +1,34 @@
 class Matriz():
+    """
+    Essa classe constroi uma matriz
+    """
     def __init__(self, lines, columns):
+        """
+        Cria uma matriz
+        :param lines: numero de linhas da matriz
+        :param columns: numero de colunas da matriz
+        """
         m = []
         for i in range(lines + 1):
             m.append([None]*(columns+1))
         self.table = m
 
     def insertPosition(self, line, column, element):
+        """
+        Esse metodo insere um elemento em uma determinada posiçao da matriz
+        :param line: linha que o elemento deve ser inserido
+        :param column: coluna que o elemento deve ser inserido
+        :param element: elemento a ser inserido
+        """
         self.table[line][column] = element
 
     def find(self, line, column):
+        """
+        acessa uma posiçao informada e retorna elemento que se encontra nela
+        :param line: linha da posiçao que procuramos
+        :param column: coluna da posiçao que procuramos
+        :return: elemento nessa posiçao
+        """
         return self.table[line][column]
 
 
@@ -22,6 +42,14 @@ class Matriz():
 
 class Maximaze():
     def __init__(self, maxWeight, maxItems, itemsWeights, itemsValues):
+        """
+        classe que calcula a melhor combinaçao de elementos com determinados pesos e valores de forma que o valor resulatante
+        seja maximo e a soma dos pesos nao exceda o limite passado
+        :param maxWeight: inteiro, limite maximo de peso
+        :param maxItems: inteiro, quantidade de itens a serem analizados
+        :param itemsWeights: lista com os pesos dos elementos
+        :param itemsValues: lista com os valores dos elementos
+        """
         self.matrix = Matriz(maxItems, maxWeight)
         self.__items = []
 
